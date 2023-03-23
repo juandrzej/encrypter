b3a10d07cac0cc57eefa9192fbdd54b336d7c840
juandrzej <ziebandrzej@gmail.com>
2023-03-23 18:28:21 +0100

    updated

--- encrypter.py
+++ encrypter.py
@@ -1,13 +1,12 @@
 from PyPDF2 import PdfReader, PdfWriter
 import sys
 
-
 file_path = sys.argv[1]
-file_pw = sys.argv[0]
+file_pw = sys.argv[2]
 
 print(file_path)
 print(file_pw)
-"""
+
 reader = PdfReader(file_path)
 writer = PdfWriter()
 
@@ -18,4 +17,3 @@ writer.encrypt(file_pw)
 
 with open(file_path, "wb") as f:
     writer.write(f)
-"""
